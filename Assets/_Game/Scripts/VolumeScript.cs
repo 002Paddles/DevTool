97dcf2be27692ff175537d2853038b2a312c548b
Juaquin Lopez <JuaquinL7@yahoo.com>
2022-07-31 22:05:16 -0500

    Added scritps for volume types

---
+++ Assets/_Game/Scripts/VolumeScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeScript : MonoBehaviour
+{
+
+    [Header("Volume Selection")]
+    [SerializeField] [Tooltip("The property you wish to give this object")] private VolumeType _volumeType;
+
+    private void Awake()
+    {
+        if(_volumeType == VolumeType.Win)
+        {
+            Debug.Log("You Win!");
+        }
+    }
+
+
+
+
+
+
+
+
+}
+                             
---
+++ Assets/_Game/Scripts/VolumeType.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum VolumeType 
+{
+    None  = 0,
+    Win,
+    Lose,
+    Hazard 
+
+}
